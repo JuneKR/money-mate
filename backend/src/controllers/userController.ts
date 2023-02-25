ba550498b52b93e7ca643b4138fe619333188b53
Ed1n <Jdei_nin230@hotmail.com>
2023-02-25 19:04:29 +0700

    modified attributes and add functions

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -51,15 +51,24 @@ export const login = async (req: Request, res: Response) => {
     res.status(200).json({ user_id, first_name, last_name, email });
 }
 
+export const logout = (req: Request, res: Response) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(400).json({ msg: "Unable to logout" })
+        }
+        res.status(200).json({ msg: "Logout successful" })
+    })
+}
+
 export const getUserProfile = async (req: Request, res: Response) => {
 
     if (!req.session.userId) {
         return res.status(401).json({ msg: "Please login to your account" });
     }
     const user = await Users.findOne({
-        attributes: ['user_id', 'first_name', 'last_name', 'username', 'date_of_birth', 'is_admin'],
+        attributes: ['User_ID', 'FirstName', 'LastName', 'DateOfBirth', 'Gender', 'RiskLevel', 'Email'],
         where: {
-            user_id: req.session.userId
+            User_ID: req.session.userId
         }
     })
     if (!user) {
@@ -68,11 +77,70 @@ export const getUserProfile = async (req: Request, res: Response) => {
     res.status(200).json(user);
 }
 
-export const logout = (req: Request, res: Response) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(400).json({ msg: "Unable to logout" })
+export const editUserProfile = async(req: Request, res: Response) => {
+    const user = await Users.findOne({
+        where: {
+            User_ID: req.params.id
         }
-        res.status(200).json({ msg: "Logout successful" })
-    })
+    });
+
+    /* Check User */
+    if(!user) {
+        return res.status(404).json({msg: "User not found"});
+    }
+
+    /* Check Password */
+    const {first_name, last_name, date_of_birth, gender, risk_level, email, password, confPassword } = req.body;
+    let hashPassword;
+    if(password === "" || password === null) {
+        hashPassword = user.Password;
+    } else {
+        hashPassword = await argon2.hash(password);
+    }
+    if (password !== confPassword) {
+        return res.status(400).json({msg: "Password and Confirm Password do not match"})
+    }
+
+    try {
+        await Users.update({
+            FirstName: first_name,
+            LastName: last_name,
+            DateOfBirth: date_of_birth,
+            Gender: gender,
+            RiskLevel: risk_level,
+            Email: email,
+            Password: hashPassword
+        }, {
+            where:{
+                User_ID: req.params.id
+            }
+        });
+        res.status(200).json({msg: "User Updated Successfully"})
+    } catch (error: any) {
+        return res.status(400).json({msg: error.message});
+    }
+}
+
+export const deleteUserProfile = async(req: Request, res: Response) => {
+    const user = await Users.findOne({
+        where: {
+            User_ID: req.params.id
+        }
+    });
+
+    /* Check User */
+    if(!user) {
+        return res.status(404).json({msg: "User not found"});
+    }
+
+    try {
+        await Users.destroy({
+            where: {
+                User_ID: req.params.id
+            }
+        })
+        res.status(200).json({msg: "User Deleted Successfully"})
+    } catch (error: any) {
+        return res.status(400).json({msg: error.message});
+    }
 }

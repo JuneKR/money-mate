c283bcf121588ad37b61b06a2a042a6fc4f8e3cf
Charan <warden.loder@gmail.com>
2023-03-02 00:24:56 +0700

    add result form with checkpoint bar

---
+++ frontend/src/components/EmergenyPageFour/emergencyResultForm.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import styles from '@/styles/Home.module.css'
+import { useRouter } from 'next/router'
+import MultiStepProgressBar from '@/components/EmergencyMultiStepProgressbar/emergencyMultiStepProgressbar';
+interface EmergencyResultFormProps {
+  title: string;
+}
+
+const EmergencyResultForm: React.FC<EmergencyResultFormProps> = ({ title }) => {
+    const router = useRouter()
+    const handleConfirmEmergencyGoal = () => {
+       router.push('/selectEmergencyPlanForm')
+     }
+     // state to keep track of the current step
+    const [currentStep, setCurrentStep] = useState(3);
+
+    const handlePrevStep = () => {
+        setCurrentStep(currentStep - 1);
+    };
+
+    const handleNextStep = () => {
+        setCurrentStep(currentStep + 1);
+    };
+  return (
+    <div style={{ width: "90%", height: "100%" }} className="sm:ml-64">
+        <div className="mb-2">
+            <div style={{display: "flex", alignItems: "center"}} className="bg-blue-200 h-24 rounded bg-gray-50 dark:bg-gray-800">
+               <p style={{ padding: "0 1rem"  }}className="text-2xl text-black dark:text-gray-500">
+                  ออมเงินเผื่อฉุกเฉิน หน้า4
+               </p>
+            </div>
+            <div style={{ width: "100%", height: "100%" }} className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+            <MultiStepProgressBar title='bar' step2Validator={true} step3Validator={false} step4Validator={false} pageNum={3}/>
+            </div>
+            
+            <div style={{ width: "100%", height: "100%",padding: "0 4rem"  }} className="rounded bg-gray-50 dark:bg-gray-800">
+                <form action="">
+                    <div style={{width: "100%", height: "50%",backgroundColor: '#E5F8FF'}} className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                        <div className="grid grid-cols-2 text-black font-bold">
+                            <div className="text-blue-800 hover:text-blue-900 p-4">เป้าหมาย</div>
+                            <div className="text-blue-800 hover:text-blue-900 p-4">ออมเงินเผื่อฉุกเฉิน</div>
+                            <div className="p-4">คุณต้องมีเงินฉุกเฉิน</div>
+                            <div className="p-4">100,000</div>
+                            <div className="p-4"> จำนวนเดือนที่ต้องการเก็บ</div>
+                            <div className="p-4">6</div>
+                            <div className="p-4">ระยะเวลาในการออม</div>
+                            <div className="p-4">5 ปี</div>
+                            <div className="p-4">เงินออมต่อเดือน</div>
+                            <div className="p-4">5000</div>
+                            <div className="p-4">ระดับความเสี่ยง</div>
+                            <div className="p-4">2 ปี</div>
+                            <div className="p-4">ผลตอบแทนที่คาดหวัง</div>
+                            <div className="p-4">4%</div>
+                        </div>    
+                    </div>
+                    <div className="flex justify-end py-5">
+                        <button onClick={handleConfirmEmergencyGoal} style={{ width: "209px",marginRight: "10px"}}className="px-4 py-2 font-bold text-black bg-gray-300 rounded shadow hover:bg-gray-400 focus:shadow-outline focus:outline-none" type="button">
+                                ย้อนกลับ
+                        </button>
+                        
+                        <button style={{ width: "209px", marginLeft: "10px"}}className="px-4 py-2 font-bold text-black bg-blue-300 rounded shadow hover:bg-blue-500 focus:shadow-outline focus:outline-none" type="button">
+                                ถัดไป
+                        </button>
+                    </div>
+               </form>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default EmergencyResultForm;

bdc32adeb39410bf86f6f11f3831ca26ffa0a290
Charan <warden.loder@gmail.com>
2023-03-02 05:53:22 +0700

    add progress  checkpoint almost done
    **unfinished**
    - button customization

--- frontend/src/components/EmergencyMultiStepProgressbar/emergencyMultiStepProgressbar.tsx
+++ frontend/src/components/EmergencyMultiStepProgressbar/emergencyMultiStepProgressbar.tsx
@@ -2,16 +2,23 @@ import React, { useRef, useEffect } from 'react';
 import { useState } from "react";
 import StepProgressBar from 'react-step-progress';
 import "react-step-progress/dist/index.css";
-
-interface EmergencyMultiStepProgressbarProps {
+import PageTwo from '@/components/EmergenyPageOne/emergencyGoalForm'
+import PageThree from '@/components/EmergenyPageTwo/cEmergencyPlanForm'
+import PageFour from '@/components/EmergenyPageThree/sEmergencyPlanForm'
+import PageFive from '@/components/EmergenyPageFour/emergencyResultForm'
+interface EmergencyMultiStepProgressbarProps  {
   title: string;
   step2Validator: boolean;
   step3Validator: boolean;
   step4Validator: boolean;
-  pageNum: number;
+  // pageNum: number;
+  nextButtonClassName?: string;
+  previousButtonClassName?: string;
+  
 }
 
-const EmergencyMultiStepProgressbar: React.FC<EmergencyMultiStepProgressbarProps> = ({ title,step2Validator,step3Validator,step4Validator,pageNum }) => {
+
+const EmergencyMultiStepProgressbar: React.FC<EmergencyMultiStepProgressbarProps> = ({ title,step2Validator,step3Validator,step4Validator,nextButtonClassName,previousButtonClassName}) => {
 
     const step2Validator2 = () => step2Validator;
     const step3Validator2= () => step3Validator;
@@ -21,40 +28,53 @@ const EmergencyMultiStepProgressbar: React.FC<EmergencyMultiStepProgressbarProps
       }
   
     const step1Content = <h1></h1>;
-    const step2Content = <h1></h1>;
-    const step3Content = <h1></h1>;
-    const step4Content = <h1></h1>;
+    const step2Content = <PageTwo title={''}/>;
+    const step3Content = <PageThree title={''}/>;
+    const step4Content = <PageFour title={''}/>;
+    const step5Content = <PageFive title={''}/>;
+  
   
     return (
-        <div style={{ width: "90%", height: "100%" }} className='text-black'>
+        <div style={{ width: "100%", height: "100%" }} className='text-black'>
         <StepProgressBar
-        startingStep={pageNum}
+        startingStep={0}
         onSubmit={onFormSubmit}
+        previousBtnName="ย้อนกลับ"
+        nextBtnName="ถัดไป"
+
+        // finishBtnName="เสร็จสิ้น"
         steps={[
           {
-            label: "Step 1",
+            label: "เลือกเป้าหมาย",
             name: "step 1",
             content: step1Content
           },
           {
-            label: "Step 2",
+            label: "สร้างเป้าหมาย",
             name: "step 2",
             content: step2Content,
             validator: step2Validator2
           },
           {
-            label: "Step 3",
+            label: "ปรับเป้าหมาย",
             name: "step 3",
             content: step3Content,
             validator: step3Validator2
           },
           {
-            label: "Step 4",
+            label: "เลือกแผน",
             name: "Step 4",
             content: step4Content,
             validator: step4Validator2
+          },
+          {
+            label: "ตรวจสอบแผน",
+            name: "Step 5",
+            content: step5Content
           }
         ]}
+        
+    
       />
       
 

1c4fd27ccb67b7f2217ac25590d92d8dd0bb1cd7
Ed1n <Jdei_nin230@hotmail.com>
2023-03-01 20:34:40 +0700

    add transaction route to saving retirement plan

--- backend/src/routes/savingRetirementPlanRoute.ts
+++ backend/src/routes/savingRetirementPlanRoute.ts
@@ -3,7 +3,10 @@ import {
     createRetirementPlan,
     getRetirementPlanById,
     editRetirementPlan,
-    deleteRetirementPlan
+    deleteRetirementPlan,
+    addTransactionToRetirementPlan,
+    getAllRetirementTransactionsByRetirementId,
+    getRetirementTransactionById
 } from "../controllers/savingRetirementPlanController";
 
 const router = express.Router();
@@ -12,5 +15,10 @@ router.get('/saving/retirement/:id', getRetirementPlanById);
 router.post('/saving/retirement', createRetirementPlan);
 router.patch('/saving/retirement/:id', editRetirementPlan);
 router.delete('/saving/retirement/:id', deleteRetirementPlan);
+
+/* Transaction */
+router.post('/saving/retirement/:id/transaction', addTransactionToRetirementPlan);
+router.get('/saving/retirement/:id/transactions', getAllRetirementTransactionsByRetirementId);
+router.get('/saving/retirement/transaction/:id', getRetirementTransactionById);
 
 export default router;

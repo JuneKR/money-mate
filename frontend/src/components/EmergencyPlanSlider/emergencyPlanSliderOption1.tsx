92b4ae6611e262e140d3956b3cb9d47074382502
Charan <warden.loder@gmail.com>
2023-03-02 23:25:34 +0700

    add option slider 1 2 3

---
+++ frontend/src/components/EmergencyPlanSlider/emergencyPlanSliderOption1.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactSlider from "react-slider";
+import "react-step-progress/dist/index.css";
+import PageTwo from '@/components/EmergenyPageOne/emergencyGoalForm'
+import PageThree from '@/components/EmergenyPageTwo/cEmergencyPlanForm'
+import PageFour from '@/components/EmergenyPageThree/sEmergencyPlanForm'
+import PageFive from '@/components/EmergenyPageFour/emergencyResultForm'
+interface EmergencyPlanSliderOption1Props  {
+  title: string;
+}
+
+
+const EmergencyPlanSliderOption1: React.FC<EmergencyPlanSliderOption1Props> = ({ title}) => {
+
+    return (
+        <div style={{ width: "100%"}} className='text-black'>
+            <ReactSlider
+            className="customSlider"
+            trackClassName="customSlider-track" 
+            thumbClassName="customSlider-thumb"
+            marks={20}
+            min={0}
+            max={100}
+            />
+            
+        </div>
+        
+    );
+  };
+  
+  export default EmergencyPlanSliderOption1;

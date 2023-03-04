c698666138dd3b6e596226ea7cbdcd6046d69cb2
Ed1n <Jdei_nin230@hotmail.com>
2023-03-04 18:33:27 +0700

    add mutual fund model

---
+++ backend/src/models/Portfolio Management/mutualFundModel.ts
@@ -0,0 +1,106 @@
+import { Model, DataTypes, Optional } from 'sequelize';
+import db from '../../config/database';
+
+interface FundAttributes {
+    Fund_ID: number;   
+    FundName: string;
+    FundSymbol: string;
+    FundType: string;
+    LastUpdate: Date;
+    CostBasis: number;
+    Nav: number;
+    ReturnRate: number;
+    ExpenseRatio: number;
+    Description: string;
+}
+
+interface FundCreationAttributes
+    extends Optional<FundAttributes, 'Fund_ID'> {}
+
+interface FundInstance
+    extends Model<FundAttributes, FundCreationAttributes>,
+    FundAttributes {
+      id: string;
+      createdAt?: Date;
+      updatedAt?: Date;
+    }
+
+const MutualFund = db.define<FundInstance>('MutualFund', {
+    Fund_ID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    FundName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [3, 50]
+        }
+    },
+    FundSymbol: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [3, 50]
+        }
+    },
+    FundType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [3, 50]
+        }
+    },
+    LastUpdate: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    CostBasis: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    Nav: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    ReturnRate: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    ExpenseRatio: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    Description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [3, 50]
+        }
+    },
+}, {
+    freezeTableName: true
+})
+
+export default MutualFund;

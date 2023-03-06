395a5c4e16443f4e7cfa0e69460cc1a989562acf
Ed1n <Jdei_nin230@hotmail.com>
2023-03-06 21:05:15 +0700

    update portfolio package controller

--- backend/src/controllers/Portfolio Management/portfolioPackageController.ts
+++ backend/src/controllers/Portfolio Management/portfolioPackageController.ts
@@ -3,6 +3,8 @@ import PackageItem from "../../models/Portfolio Management/PackageItem";
 import MutualFund from "../../models/Portfolio Management/mutualFundModel";
 import { Request, Response } from "express";
 
+
+/* Portfolio Package */
 export const getAllPortfolioPackages = async(req: Request, res: Response) => {
     
     try {
@@ -30,18 +32,19 @@ export const getAllPortfolioPackages = async(req: Request, res: Response) => {
 }
 
 export const getPortfolioPackageById = async(req: Request, res: Response) => {
-    const portfolioPackage = await PortfolioPackage.findOne({
-        where: {
-            Package_ID: req.params.id
-        }
-    });
-
-    /* Check Plan */
-    if(!portfolioPackage) {
-        return res.status(404).json({msg: `Portfolio Package id: ${req.params.id} not found!`});
-    }
     
     try {
+        const portfolioPackage = await PortfolioPackage.findOne({
+            where: {
+                Package_ID: req.params.id
+            }
+        });
+
+        /* Check Plan */
+        if(!portfolioPackage) {
+            return res.status(404).json({msg: `Portfolio Package id: ${req.params.id} not found!`});
+        }
+
         const response = await PortfolioPackage.findAll({
             attributes:[
                 'PackageName',
@@ -58,4 +61,141 @@ export const getPortfolioPackageById = async(req: Request, res: Response) => {
     } catch (error: any) {
         res.status(500).json({msg: error.message});
     }
+
+}
+
+export const editPortfolioPackageInfo = async(req: Request, res: Response) => {
+    
+    try {
+        const portfolioPackage = await PortfolioPackage.findOne({
+            where: {
+                Package_ID: req.params.id
+            }
+        });
+
+        /* Check User */
+        if(!portfolioPackage) {
+            return res.status(404).json({msg: `Portfolio Package with id: ${req.params.id} not found`});
+        }
+
+        const {   
+            package_name,
+            last_update,
+            risk_level,
+            investment_type,
+            estimated_return_rate,
+        } = req.body;
+
+        await PortfolioPackage.update({
+            Package_ID: package_name,
+            PackageName: last_update,
+            LastUpdate: last_update,
+            RiskLevel: risk_level,
+            InvestmentType: investment_type,
+            EstimatedReturnRate: estimated_return_rate
+        }, {
+            where:{
+                Package_ID: req.params.id
+            }
+        });
+        res.status(200).json({msg: `Portfolio Package id: ${req.params.id} Updated Successfully`})
+    } catch (error: any) {
+        return res.status(400).json({msg: error.message});
+    }
+}
+
+/* Portfolio Package Item */
+export const getPortfolioPackageAllocation = async(req: Request, res: Response) => {
+}
+
+export const updatePortfolioPackageAllocation = async(req: Request, res: Response) => {
+}
+
+/* Mutual Fund */
+export const getMutualFundByFundId = async(req: Request, res: Response) => {
+
+    try {
+        const mutualFund = await MutualFund.findOne({
+            where: {
+                Fund_ID: req.params.id
+            }
+        });
+
+        /* Check Plan */
+        if(!mutualFund) {
+            return res.status(404).json({msg: `Mutual Fund id: ${req.params.id} not found!`});
+        }
+
+        const response = await MutualFund.findAll({
+            attributes:[
+                'FundName',
+                'FundSymbol',
+                'FundType',
+                'LastUpdate',
+                'CostBasis',
+                'Nav',
+                'ReturnRate',
+                'ExpenseRatio',
+                'Description',
+            ],
+            where: {
+                Fund_ID: req.params.id,
+            }
+        });
+        res.status(200).json(response);
+    } catch (error: any) {
+        res.status(500).json({msg: error.message});
+    }
+
+}
+
+export const getAllMutualFundByPackageId = async(req: Request, res: Response) => {
+}
+
+export const editMutualFundInfo = async(req: Request, res: Response) => {
+
+    try {
+        const mutualFund = await MutualFund.findOne({
+            where: {
+                Fund_ID: req.params.id
+            }
+        });
+
+        /* Check User */
+        if(!mutualFund) {
+            return res.status(404).json({msg: `Mutual Fund id: ${req.params.id} not found`});
+        }
+
+        const {   
+            fund_name,
+            fund_symbol,
+            fund_type,
+            last_update,
+            cost_basis,
+            nav,
+            return_rate,
+            expense_ratio,
+            description
+        } = req.body;
+
+        await MutualFund.update({
+            FundName: fund_name,
+            FundSymbol: fund_symbol,
+            FundType: fund_type,
+            LastUpdate: last_update,
+            CostBasis: cost_basis,
+            Nav: nav,
+            ReturnRate: return_rate,
+            ExpenseRatio: expense_ratio,
+            Description: description,
+        }, {
+            where:{
+                Fund_ID: req.params.id
+            }
+        });
+        res.status(200).json({msg: `Mutual Fund id: ${req.params.id} Updated Successfully`})
+    } catch (error: any) {
+        return res.status(400).json({msg: error.message});
+    }
+
 }

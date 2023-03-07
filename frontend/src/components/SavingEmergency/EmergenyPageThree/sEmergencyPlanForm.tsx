21d104f7edb8f9a3ba59231219c4bbbecb2411e8
Charan <warden.loder@gmail.com>
2023-03-08 03:17:34 +0700

    delete grid form page component

--- frontend/src/components/SavingEmergency/EmergenyPageThree/sEmergencyPlanForm.tsx
+++ frontend/src/components/SavingEmergency/EmergenyPageThree/sEmergencyPlanForm.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import { FormDataprops } from "../EmergencyMultiStepProgressbar/emergencyMultiStepProgressbar";
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import EmergencyPlanDataTable from '@/components/SavingEmergency/EmergencyPlanGridTable/emargencyPlanGridTable'
 
 interface SEmergencyPlanFormProps {
   title: string;
@@ -51,29 +51,6 @@ const SEmergencyPlanForm: React.FC<FormDataprops> = ({formData, setFormData}) =>
     function valueLabelFormat(value: number) {
       return marks.findIndex((mark) => mark.value === value) + 1;
     }
-    const columns: GridColDef[] = [
-      { field: 'plan', headerName: 'เป้าหมาย', width: 70 },
-      { field: 'amount', headerName: 'จำนวนเงิน', width: 130 },
-      { field: 'monthlyDeposit', headerName: 'เงินออมต่อเดือน', width: 130 },
-      { field: 'returnRate', headerName: 'ผลตอบแทน', width: 70 },
-      { field: 'period', headerName: 'ระยะเวลา', width: 70 },
-      // {
-      //   field: 'fullName',
-      //   headerName: 'Full name',
-      //   description: 'This column has a value getter and is not sortable.',
-      //   sortable: false,
-      //   width: 160,
-      //   valueGetter: (params: GridValueGetterParams) =>
-      //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-      // },
-    ];
-    const rows = [
-      { id: 1, plan: 'A', amount: 100000, monthlyDeposit: 1500, returnRate: '1%', period: '2y' },
-      { id: 2, plan: 'A', amount: 100000, monthlyDeposit: 1500, returnRate: '4%', period: '4.9y' },
-      { id: 3, plan: 'A', amount: 100000, monthlyDeposit: 1500, returnRate: '4%', period: '5y' },
-      { id: 4, plan: 'A', amount: 100000, monthlyDeposit: 1500, returnRate: '4%', period: '4.5y' },
-      { id: 5, plan: 'A', amount: 100000, monthlyDeposit: 1500, returnRate: '4%', period: '5y' }
-    ];
   return (
     <div className="py-20">
       <div style={{ width: "100%", height: "100%",padding: "0 4rem"  }} className="rounded bg-gray-50 dark:bg-gray-800">
@@ -153,14 +130,8 @@ const SEmergencyPlanForm: React.FC<FormDataprops> = ({formData, setFormData}) =>
                     <h1>เลือกแผนของคุณด้วยผลตอบแทนที่คุณรับได้</h1>
                   </div>
                   <div  className="block w-full px-3 py-2 text-sm placeholder-gray-500 border border-gray-300 rounded-md shadow-sm">
-                    <div style={{ height: 400, width: '100%' }} className=" bg-gray-50 dark:bg-gray-800">
-                    <DataGrid
-                      rows={rows}
-                      columns={columns}
-                      // pageSize={5}
-                      // rowsPerPageOptions={[5]}
-                      checkboxSelection
-                    />
+                    <div>
+                     <EmergencyPlanDataTable title={"my table1"}/>
                     </div>
                   </div>
                </form>

5e02f8b29acde5602704013f28ca2ae6e6f489d8
Charan <warden.loder@gmail.com>
2023-03-09 05:21:39 +0700

    delete old nav

--- frontend/src/pages/savingSelectionPage.tsx
+++ frontend/src/pages/savingSelectionPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from '@/styles/Home.module.css'
 import Sidebar from '@/components/Sidebar'
-import Navbar from '@/components/Navbar'
 import Body from '@/components/savingSelectionPageButtons'
 
 const SavingSelectionPage = () => {

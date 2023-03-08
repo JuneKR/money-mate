47612b20e116bd111efe582569226952af33958b
Ed1n <Jdei_nin230@hotmail.com>
2023-03-08 16:51:38 +0700

    update portfolio package route

--- backend/src/routes/Portfolio Management/portfolioPackageRoute.ts
+++ backend/src/routes/Portfolio Management/portfolioPackageRoute.ts
@@ -8,7 +8,7 @@ import {
     getMutualFundByFundId,
     getAllMutualFundByPackageId,
     editMutualFundInfo
-} from '../../controllers/Portfolio Management/portfolioPackageController';
+} from '../../controllers/portfolio management/portfolioPackageController';
 
 const router = express.Router();
 

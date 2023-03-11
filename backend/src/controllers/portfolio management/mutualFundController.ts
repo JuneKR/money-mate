047d6ff25ebf84c19851846b76ccae13ff98fe59
Ed1n <Jdei_nin230@hotmail.com>
2023-03-11 15:45:20 +0700

    update attribute in mutual fund controller

--- backend/src/controllers/portfolio management/mutualFundController.ts
+++ backend/src/controllers/portfolio management/mutualFundController.ts
@@ -1,5 +1,5 @@
 import MutualFund from "../../models/portfolio management/mutualFundModel";
-import PackageItem from "../../models/portfolio management/PackageItem";
+import PackageItem from "../../models/portfolio management/packageItem";
 import { Request, Response } from "express";
 
 /* Mutual Fund */
@@ -8,27 +8,37 @@ export const createMutualFund = async(req: Request, res: Response) => {
     try {
         const 
         {   
-            fund_name,
-            fund_symbol,
-            fund_type,
             last_update,
-            cost_basis,
+            fund_name,
+            fund_abbr_name,
+            risk_spectrum,
+            policy_desc,
+            spec_code,
+            spec_desc,
             nav,
-            return_rate,
-            expense_ratio,
-            description,
+            minimum_investment_amount,
+            minimum_additional_amount,
+            one_year_returns,
+            three_year_returns,
+            five_year_returns,
+            ytd_returns,
         } = req.body;
 
         await MutualFund.create({ 
-            FundName: fund_name,
-            FundSymbol: fund_symbol,
-            FundType: fund_type,
             LastUpdate: last_update,
-            CostBasis: cost_basis,
-            Nav: nav,
-            ReturnRate: return_rate,
-            ExpenseRatio: expense_ratio,
-            Description: description,
+            FundName: fund_name,
+            FundAbbrName: fund_abbr_name,
+            RiskSpectrum: risk_spectrum,
+            PolicyDesc: policy_desc,
+            SpecCode: spec_code,
+            SpecDesc: spec_desc,
+            NAV: nav,
+            MinimumInvestmentAmount: minimum_investment_amount,
+            MinimumAdditionalAmount: minimum_additional_amount,
+            oneYearReturns: one_year_returns,
+            threeYearReturns: three_year_returns,
+            fiveYearReturns: five_year_returns,
+            YTDReturns: ytd_returns,
         });
         res.status(201).json({msg: "Successful Create new mutual fund"});
     } catch (error: any) {
@@ -52,15 +62,20 @@ export const getMutualFundByFundId = async(req: Request, res: Response) => {
 
         const response = await MutualFund.findAll({
             attributes:[
-                'FundName',
-                'FundSymbol',
-                'FundType',
                 'LastUpdate',
-                'CostBasis',
-                'Nav',
-                'ReturnRate',
-                'ExpenseRatio',
-                'Description',
+                'FundName',
+                'FundAbbrName',
+                'RiskSpectrum',
+                'PolicyDesc',
+                'SpecCode',
+                'SpecDesc',
+                'NAV',
+                'MinimumInvestmentAmount',
+                'MinimumAdditionalAmount',
+                'oneYearReturns',
+                'threeYearReturns',
+                'fiveYearReturns',
+                'YTDReturns',
             ],
             where: {
                 Fund_ID: req.params.id,
@@ -82,33 +97,43 @@ export const editMutualFundInfo = async(req: Request, res: Response) => {
             }
         });
 
-        /* Check User */
+        /* Check Mutual Fund */
         if(!mutualFund) {
             return res.status(404).json({msg: `Mutual Fund id: ${req.params.id} not found`});
         }
 
         const {   
-            fund_name,
-            fund_symbol,
-            fund_type,
             last_update,
-            cost_basis,
+            fund_name,
+            fund_abbr_name,
+            risk_spectrum,
+            policy_desc,
+            spec_code,
+            spec_desc,
             nav,
-            return_rate,
-            expense_ratio,
-            description
+            minimum_investment_amount,
+            minimum_additional_amount,
+            one_year_returns,
+            three_year_returns,
+            five_year_returns,
+            ytd_returns,
         } = req.body;
 
         await MutualFund.update({
-            FundName: fund_name,
-            FundSymbol: fund_symbol,
-            FundType: fund_type,
             LastUpdate: last_update,
-            CostBasis: cost_basis,
-            Nav: nav,
-            ReturnRate: return_rate,
-            ExpenseRatio: expense_ratio,
-            Description: description,
+            FundName: fund_name,
+            FundAbbrName: fund_abbr_name,
+            RiskSpectrum: risk_spectrum,
+            PolicyDesc: policy_desc,
+            SpecCode: spec_code,
+            SpecDesc: spec_desc,
+            NAV: nav,
+            MinimumInvestmentAmount: minimum_investment_amount,
+            MinimumAdditionalAmount: minimum_additional_amount,
+            oneYearReturns: one_year_returns,
+            threeYearReturns: three_year_returns,
+            fiveYearReturns: five_year_returns,
+            YTDReturns: ytd_returns,
         }, {
             where:{
                 Fund_ID: req.params.id
@@ -140,15 +165,20 @@ export const getAllMutualFundByPackageId = async(req: Request, res: Response) =>
 
         const response = await PackageItem.findAll({
             attributes:[
-                'FundName',
-                'FundSymbol',
-                'FundType',
                 'LastUpdate',
-                'CostBasis',
-                'Nav',
-                'ReturnRate',
-                'ExpenseRatio',
-                'Description',
+                'FundName',
+                'FundAbbrName',
+                'RiskSpectrum',
+                'PolicyDesc',
+                'SpecCode',
+                'SpecDesc',
+                'NAV',
+                'MinimumInvestmentAmount',
+                'MinimumAdditionalAmount',
+                'oneYearReturns',
+                'threeYearReturns',
+                'fiveYearReturns',
+                'YTDReturns',
             ],
             where: {
                 Package_ID: req.params.id

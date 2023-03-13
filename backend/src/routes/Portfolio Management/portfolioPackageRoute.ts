2f244cfab0510530c68e18ab55d508ad6ee2a8aa
Ed1n <Jdei_nin230@hotmail.com>
2023-03-13 14:42:18 +0700

    add new route

--- backend/src/routes/Portfolio Management/portfolioPackageRoute.ts
+++ backend/src/routes/Portfolio Management/portfolioPackageRoute.ts
@@ -1,19 +1,25 @@
 import express from "express";
 import {
+    createPortfolioPackage,
     getAllPortfolioPackages,
     getPortfolioPackageById,
     editPortfolioPackageInfo,
     getAllPortfolioPackageAllocationByPackageId,
     editPortfolioPackageAllocationByPackageId,
+    addMutualFundToPackage
 } from '../../controllers/portfolio management/portfolioPackageController';
 
 const router = express.Router();
 
 /* Portfolio Package */
+router.post('/portfolio/package', createPortfolioPackage);
 router.get('/portfolio/packages', getAllPortfolioPackages);
 router.get('/portfolio/package/:id', getPortfolioPackageById);
 router.patch('/portfolio/package/:id', editPortfolioPackageInfo);
 router.get('/portfolio/package/:id/allocations', getAllPortfolioPackageAllocationByPackageId);
 router.patch('/portfolio/package/:id/allocations', editPortfolioPackageAllocationByPackageId);
+
+/* Add New Mutual Fund to Package */
+router.post('/portfolio/package/fund', addMutualFundToPackage);
 
 export default router;

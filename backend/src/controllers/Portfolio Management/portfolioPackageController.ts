c685ede486c4b315e02d3b3db6cb7fc540a8710b
Ed1n <Jdei_nin230@hotmail.com>
2023-03-16 16:59:57 +0700

    update package controller

--- backend/src/controllers/Portfolio Management/portfolioPackageController.ts
+++ backend/src/controllers/Portfolio Management/portfolioPackageController.ts
@@ -97,7 +97,7 @@ export const editPortfolioPackageInfo = async(req: Request, res: Response) => {
             }
         });
 
-        /* Check User */
+        /* Check Package */
         if(!portfolioPackage) {
             return res.status(404).json({msg: `Portfolio Package with id: ${req.params.id} not found`});
         }
@@ -111,12 +111,11 @@ export const editPortfolioPackageInfo = async(req: Request, res: Response) => {
         } = req.body;
 
         await PortfolioPackage.update({
-            Package_ID: package_name,
-            PackageName: last_update,
+            PackageName: package_name,
             LastUpdate: last_update,
             RiskSpectrum: risk_spectrum,
             InvestmentType: investment_type,
-            ReturnRate: return_rate
+            ReturnRate: return_rate,
         }, {
             where:{
                 Package_ID: req.params.id
@@ -158,8 +157,10 @@ export const getAllPortfolioPackageAllocationByPackageId = async(req: Request, r
     }
 }
 
+// Change mutual fund allocation in portfolio package
 export const editPortfolioPackageAllocationByPackageId = async(req: Request, res: Response) => {
     try {
+        // Find PackageItem with Portfolio Package ID
         const packageItem = await PackageItem.findOne({
             where: {
                 Package_ID: req.params.id
@@ -168,7 +169,7 @@ export const editPortfolioPackageAllocationByPackageId = async(req: Request, res
 
         /* Check Plan */
         if(!packageItem) {
-            return res.status(404).json({msg: `Portfolio package with id ${req.params.id} not found`});
+            return res.status(404).json({msg: `Package item with Portfolio id ${req.params.id} not found`});
         }
 
         const {   
@@ -213,9 +214,69 @@ export const addMutualFundToPackage = async(req: Request, res: Response) => {
             Fund_ID: fund_id,
             AllocationRatio: allocation_ratio
         });
-        res.status(201).json({msg: "Successful Create new portfolio package"});
+        res.status(201).json({msg: "Successful add mutual fund and allocation to portfolio package"});
     } catch (error: any) {
         res.status(500).json({msg: error.message});
     }
 }
 /* Calculate Return Rate from Mutual Fund Allocation Ratio */
+export const calculatePortfolioPackageReturns = async(req: Request, res: Response) => {
+    try { 
+        const portfolioPackage = await PortfolioPackage.findOne({
+            where: {
+                Package_ID: req.params.id
+            }
+        });
+        if(!portfolioPackage) {
+            return res.status(404).json({msg: `Portfolio Package with id ${req.params.id} not found`});
+        }
+
+        const packageItem = await PackageItem.findAll({
+            attributes:[
+                'Package_ID',
+                'Fund_ID',
+                'AllocationRatio'
+            ],
+            where: {
+                Package_ID: req.params.id
+            }
+        });
+
+        let totalMutualFundReturn = 0;
+ 
+        for (const item of packageItem) {
+            const FundId = item.Fund_ID;
+            //Check Mutual Fund
+            const mutualFund = await MutualFund.findByPk(FundId);
+            if(!mutualFund) {
+                return res.status(404).json({msg: `Mutual Fund with id ${FundId} not found`});
+            } else {
+                const mutualFundAllocation = item.AllocationRatio;
+                let mutualFundReturns = 0;
+                // Calculate mutual fund returns from the oldest available returns
+                if (mutualFund.fiveYearReturns) {
+                    mutualFundReturns = mutualFund.fiveYearReturns;
+                } else if (mutualFund.threeYearReturns) {
+                    mutualFundReturns = mutualFund.threeYearReturns;
+                } else if (mutualFund.oneYearReturns) {
+                    mutualFundReturns = mutualFund.oneYearReturns;
+                } else {
+                    mutualFundReturns = mutualFund.YTDReturns;
+                }
+                totalMutualFundReturn += (mutualFundReturns / 100) * mutualFundAllocation;
+            }
+        }
+
+        // update portfolio package return rate
+        await PortfolioPackage.update({
+            ReturnRate: totalMutualFundReturn
+        }, {
+            where:{
+                Package_ID: req.params.id
+            }
+        });
+        res.status(201).json({msg: "Successful calculate portfolio package returns"});
+    } catch (error: any) {
+        res.status(500).json({msg: error.message});
+    }
+}

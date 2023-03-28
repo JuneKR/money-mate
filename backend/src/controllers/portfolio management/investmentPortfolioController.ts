ce53aed2b1c7cec4482d9c4acdb2801a17385c78
Ed1n <Jdei_nin230@hotmail.com>
2023-03-29 01:35:34 +0700

    update investment portfolio

--- backend/src/controllers/portfolio management/investmentPortfolioController.ts
+++ backend/src/controllers/portfolio management/investmentPortfolioController.ts
@@ -3,6 +3,9 @@ import PortfolioItem from "../../models/portfolio management/investment portfoli
 import MutualFund from "../../models/portfolio management/mutualFundModel";
 import InvestmentTransaction from "../../models/portfolio management/investmentTransactionModel";
 import { Request, Response } from "express";
+import SavingEmergencyPlan from "../../models/savingEmergencyPlanModel";
+import SavingRetirementPlan from "../../models/savingRetirementPlanModel";
+import GoalBasedSavingPlan from "../../models/goalBasedSavingPlanModel";
 
 
 /* Investment Portfolio */
@@ -246,6 +249,7 @@ export const calculateInvestmentROI = async(req: Request, res: Response) => {
 export const addInvestmentTransaction = async(req: Request, res: Response) => {
 
     try {
+        // only just the input with plan id is better?
         const { transaction_date, policy_desc, fund_abbr_name, amount, type } = req.body;
 
         /* Check Investment Portfolio */
@@ -258,24 +262,159 @@ export const addInvestmentTransaction = async(req: Request, res: Response) => {
             return res.status(404).json({msg: "Investment Portfolio not found"});
         }
 
-        /* recalculated of investment portfolio total value */
-        let totalValue: number = 0;
-        if (type === 'deposit') {
-            /*
-                ...
-            */
-            await InvestmentPortfolio.update({
-                TotalValue: totalValue
-            }, {
-                where: {
-                    Portfolio_ID: req.params.id
-                } 
-            })    
+        // Check the saving plan of portfolio
+        const allSavingPlanId = await InvestmentPortfolio.findOne({
+            attributes:[
+                'Emergency_ID',
+                'Goal_ID',
+                'Retirement_ID'
+            ],
+            where: {
+                Portfolio_ID: req.params.id,
+            }
+        })
+        
+        let savingPlan;
+        let savingPlanCategory = '';
+        let savingPlanId = 0;
+        if (!allSavingPlanId?.Emergency_ID && !allSavingPlanId?.Goal_ID && !allSavingPlanId) {
+            return res.status(404).json({msg: "Saving plan id not found in an investment portfolio"});
         }
-        else if (type === 'withdrawal') {
-            /*
-                ...
-            */
+        else if(allSavingPlanId.Emergency_ID) {
+            savingPlanCategory = 'Emergency'
+            savingPlan = await SavingEmergencyPlan.findOne({
+                where: {
+                    Emergency_ID: allSavingPlanId.Emergency_ID,
+                }
+            })
+            if(!savingPlan) {
+                return res.status(404).json({msg: "Saving plan not found"});
+            }
+
+            savingPlanId = savingPlan.Emergency_ID;
+        }
+        else if(allSavingPlanId.Retirement_ID) {
+            savingPlanCategory = 'Retirement'
+            savingPlan = await SavingRetirementPlan.findOne({
+                where: {
+                    Retirement_ID: allSavingPlanId.Retirement_ID,
+                }
+            })
+            if(!savingPlan) {
+                return res.status(404).json({msg: "Saving plan not found"});
+            }
+            
+            savingPlanId = savingPlan.Retirement_ID;
+        }
+        else if(allSavingPlanId.Goal_ID) {
+            savingPlanCategory = 'Goal'
+            savingPlan = await GoalBasedSavingPlan.findOne({
+                where: {
+                    Goal_ID: allSavingPlanId.Goal_ID,
+                }
+            })
+            if(!savingPlan) {
+                return res.status(404).json({msg: "Saving plan not found"});
+            }
+            
+            savingPlanId = savingPlan.Goal_ID;
+        }
+
+        if(!savingPlan) {
+            return res.status(404).json({msg: "Saving plan not found"});
+        }
+        // [/] 1.Check whether it is buy or sell
+        // [/] 2.Reduce or Increase (recalculated) total balance of saving plan
+        // [/] 2.1 Check what the plan is for reculated (saving plan is not null)
+        // [/] 2.2 Check what the plan has enough money to buy 
+        // [/] 2.3 update total balance of saving plan
+        // [/] 3.recalculated total value of investment portfolio 
+        // [/] 3.1 Increase or Decrease total value of investment portfolio
+        // [] 4.adjust the allocation ratio of portfolio item
+        // [/] 5.add new transaction history
+        let totalValue: number = investmentPortfolio.TotalValue;
+        let totalBalance: number = savingPlan.TotalBalance;
+        if (type === 'buy') {   
+            // Check whether plan has enough total balance and update total balance of each plan
+            if (savingPlan.TotalBalance >= amount) {
+                totalBalance -= parseFloat(amount);
+                totalValue += parseFloat(amount);
+                if(savingPlanCategory === 'Emergency') {
+                    await SavingEmergencyPlan.update({
+                        TotalBalance: totalBalance
+                    }, 
+                    {
+                        where: {
+                            Emergency_ID: savingPlanId
+                        } 
+                    })
+                }
+                else if(savingPlanCategory === 'Retirement') {
+                    await SavingRetirementPlan.update({
+                        TotalBalance: totalBalance
+                    }, 
+                    {
+                        where: {
+                            Retirement_ID: savingPlanId
+                        } 
+                    })
+                }
+                else if(savingPlanCategory === 'Retirement') {
+                    await GoalBasedSavingPlan.update({
+                        TotalBalance: totalBalance
+                    }, 
+                    {
+                        where: {
+                            Goal_ID: savingPlanId
+                        } 
+                    })
+                }
+            }   
+            else {
+                return res.status(404).json({msg: `Total Balance of Saving plan name ${savingPlan.PlanName} is not enough to buy`});
+            } 
+        }
+        else if (type === 'sell') {
+            // Check whether plan has enough total balance and update total balance of each plan
+            if (savingPlan.TotalBalance >= amount) {
+                totalBalance += parseFloat(amount);
+                totalValue -= parseFloat(amount);
+                if(savingPlanCategory === 'Emergency') {
+                    await SavingEmergencyPlan.update({
+                        TotalBalance: totalBalance
+                    }, 
+                    {
+                        where: {
+                            Emergency_ID: savingPlanId
+                        } 
+                    })
+                }
+                else if(savingPlanCategory === 'Retirement') {
+                    await SavingRetirementPlan.update({
+                        TotalBalance: totalBalance
+                    }, 
+                    {
+                        where: {
+                            Retirement_ID: savingPlanId
+                        } 
+                    })
+                }
+                else if(savingPlanCategory === 'Retirement') {
+                    await GoalBasedSavingPlan.update({
+                        TotalBalance: totalBalance
+                    }, 
+                    {
+                        where: {
+                            Goal_ID: savingPlanId
+                        } 
+                    })
+                }
+            }    
+            else {
+                return res.status(404).json({msg: `Total Balance of Saving plan name ${savingPlan.PlanName} is not enough`});
+            }
+
+            // Update Total Value of Investment Portfolio
             await InvestmentPortfolio.update({
                 TotalValue: totalValue
             }, {
@@ -283,19 +422,21 @@ export const addInvestmentTransaction = async(req: Request, res: Response) => {
                     Portfolio_ID: req.params.id
                 } 
             })  
+
+            // Update Portfolio Allocation
         }
         else {
             return res.status(404).json({msg: `Transaction type error with ${type}`});
         }
         
-        /* Create Investment Transaction */ 
+        /* Add Investment Transaction */ 
         await InvestmentTransaction.create({
             TransactionDate: transaction_date,
             PolicyDesc: policy_desc,
             FundAbbrName: fund_abbr_name,
             Amount: amount,
             Type: type,
-            Portfolio_ID: investmentPortfolio.Package_ID
+            Portfolio_ID: investmentPortfolio.Portfolio_ID
         });
 
         res.status(201).json({msg: "Investment transaction history is recorded"});
--- backend/src/routes/Portfolio Management/investmentPortfolioRoute.ts
+++ backend/src/routes/Portfolio Management/investmentPortfolioRoute.ts
@@ -7,7 +7,7 @@ import {
     deleteInvestmentPortfolio,
     getInvestmentPortfolioAllocationByPortfolioId,
     editInvestmentPortfolioAllocationByPortfolioId,
-    // addInvestmentTransaction, 'Not finished yet!!'
+    addInvestmentTransaction, 
     getAllInvestmentTransactionsByPortfolioId,
     getInvestmentTransactionById
 } from '../../controllers/portfolio management/investmentPortfolioController';
@@ -16,13 +16,14 @@ const router = express.Router();
 
 /* Investment Portfolio */
 router.post('/investment/portfolio', createInvestmentPortfolio);
-router.get('/user/:id/investment/portfolios/', getAllInvestmentPortfolioByUserId);
-router.get('/investment/portfolio', getInvestmentPortfolioById);
+router.get('/user/:id/investment/portfolios', getAllInvestmentPortfolioByUserId);
+router.get('/investment/portfolio/:id', getInvestmentPortfolioById);
 router.patch('/investment/portfolio/:id', editInvestmentPortfolio);
 router.delete('/investment/portfolio/:id', deleteInvestmentPortfolio)
 
 router.get('/investment/portfolio/:id/allocation', getInvestmentPortfolioAllocationByPortfolioId);
 router.patch('/investment/portfolio/:id/allocation', editInvestmentPortfolioAllocationByPortfolioId);
+router.post('/investment/portfolio/:id/transaction', addInvestmentTransaction);
 router.get('/investment/:id/transaction', getAllInvestmentTransactionsByPortfolioId);
 router.get('/investment/transaction/:id', getInvestmentTransactionById);
 

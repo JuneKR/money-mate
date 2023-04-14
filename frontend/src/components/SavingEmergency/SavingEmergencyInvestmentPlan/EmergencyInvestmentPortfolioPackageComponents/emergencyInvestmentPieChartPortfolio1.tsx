16be436ef47d6bc48bd5ffde90489c106a6b93af
Ed1n <Jdei_nin230@hotmail.com>
2023-04-14 18:27:13 +0700

    update hover in pie chart

--- frontend/src/components/SavingEmergency/SavingEmergencyInvestmentPlan/EmergencyInvestmentPortfolioPackageComponents/emergencyInvestmentPieChartPortfolio1.tsx
+++ frontend/src/components/SavingEmergency/SavingEmergencyInvestmentPlan/EmergencyInvestmentPortfolioPackageComponents/emergencyInvestmentPieChartPortfolio1.tsx
@@ -1,42 +1,79 @@
 import React, { useRef, useEffect } from 'react';
 import { Pie } from 'react-chartjs-2';
-import {Chart, ArcElement} from 'chart.js'
+import { Chart, ArcElement } from 'chart.js';
+import tinycolor from 'tinycolor2';
 Chart.register(ArcElement);
 
-interface EmergencyInvestmentPieChartPortfolio1Props {
-    title: string;
+interface PortfolioItem {
+  Portfolio_ID: number;
+  Fund_ID: number;
+  PolicyDesc: string;
+  FundAbbrName: string;
+  OneYearReturns: number;
+  AllocationRatio: number;
 }
 
-const EmergencyInvestmentPieChartPortfolio1: React.FC<EmergencyInvestmentPieChartPortfolio1Props> = ({ title}) => {
-    const chartRef = useRef(null);
+interface EmergencyInvestmentPieChartPortfolio1Props {
+  title: string;
+  investmentPortfolioAllocation: PortfolioItem[];
+}
 
-    useEffect(() => {
-      if (chartRef.current) {
-          const chartInstance = (chartRef.current as any).chartInstance;
-        if (chartInstance) {
-          chartInstance.destroy();
-        }
+const EmergencyInvestmentPieChartPortfolio1: React.FC<EmergencyInvestmentPieChartPortfolio1Props> = (props) => {
+  const { title, investmentPortfolioAllocation } = props;
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const chartInstance = (chartRef.current as any).chartInstance;
+      if (chartInstance) {
+        chartInstance.destroy();
       }
-    });
-
-    const data = {
-      labels: ['Red', 'Green', 'Yellow'],
-      datasets: [
-        {
-          data: [300, 50, 100],
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-        }
-      ]
-    };
-    const options = {
-      responsive: true,
-      maintainAspectRatio: true,	// Don't maintain w/h ratio
-      
     }
-    return (
-      <div><Pie ref={chartRef} data={data} options={options}/></div>
-    );
+  });
+
+  const getRandomColor = (tone: string) => {
+    const randomSaturation = Math.floor(Math.random() * 20);
+    const randomLightness = Math.floor(Math.random() * 20);
+    const randomColor = tinycolor(tone).saturate(randomSaturation).lighten(randomLightness);
+    return randomColor.toHexString();
+  };
+  
+  const data = {
+    labels: investmentPortfolioAllocation.map((item) => item.FundAbbrName),
+    datasets: [
+      {
+        data: investmentPortfolioAllocation.map((item) => item.AllocationRatio),
+        backgroundColor: investmentPortfolioAllocation.map(() => getRandomColor("#6259E8")),
+        hoverBackgroundColor: investmentPortfolioAllocation.map(() => getRandomColor("#1D1D41")),
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (context: any) {
+            const label = context.dataset.labels[context.dataIndex];
+            const value = context.dataset.data[context.dataIndex];
+            return `${label}: ${value}`;
+          }
+        },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <Pie
+        ref={chartRef}
+        data={data}
+        options={options}
+      />
+    </div>
+  );
 };
 
 export default EmergencyInvestmentPieChartPortfolio1;

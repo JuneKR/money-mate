28c62cbf628a9ea2a13fdad47e685ded72830cc0
Charan <warden.loder@gmail.com>
2023-04-21 09:53:03 +0700

    Update _document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         rel="stylesheet"
         href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
       /> */}
-      <script src="https://cdn.tailwindcss.com"></script>
+      {/* <script src="https://cdn.tailwindcss.com"></script> */}
       <meta charSet="UTF-8" />
 
       <Head />

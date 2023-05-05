c74c3e36f09c434993ee03c6c0d6d77760dcf5ad
Ed1n <Jdei_nin230@hotmail.com>
2023-05-05 22:13:47 +0700

    remove unused style and import material

--- frontend/src/components/SavingForGoal/SavingGoalDashBoardComponents/sGoalDashBoardModalForm.tsx
+++ frontend/src/components/SavingForGoal/SavingGoalDashBoardComponents/sGoalDashBoardModalForm.tsx
@@ -1,28 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
-import { useCallback } from "react";
-import EditIcon from "@mui/icons-material/Edit";
-import { SavingEmergencyPlan } from "@/pages/EmergencyPages/emergencyDashboard";
+import React from "react";
 
 interface SGoalDashBoardModalFormProps {
   title: string;
   savingGoal: any;
 }
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "50%",
-  bgcolor: "#1D1D41",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 
 function yearsToYearsMonthsDays(value: string) {
   const totalDays = Number(value) * 365;

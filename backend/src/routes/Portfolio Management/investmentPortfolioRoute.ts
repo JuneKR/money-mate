64218f1974c74ff4caa0edf8cf45594438ff2fd1
Ed1n <Jdei_nin230@hotmail.com>
2023-05-13 22:01:01 +0700

    adjust api

--- backend/src/routes/Portfolio Management/investmentPortfolioRoute.ts
+++ backend/src/routes/Portfolio Management/investmentPortfolioRoute.ts
@@ -23,25 +23,25 @@ import {
 const router = express.Router();
 
 /* Investment Portfolio */
-router.post('/investment/portfolio', createInvestmentPortfolio);
-router.get('/user/:id/investment/portfolios', getAllInvestmentPortfolioByUserId);
-router.get('/investment/portfolio/:id', getInvestmentPortfolioById);
-router.patch('/investment/portfolio/:id', editInvestmentPortfolio);
-router.delete('/investment/portfolio/:id', deleteInvestmentPortfolio)
-router.get('/emergency/:id/investment/portfolio', getInvestmentPortfolioByEmergencyId);
-router.get('/goal/:id/investment/portfolio', getInvestmentPortfolioByGoalId);
-router.get('/retirement/:id/investment/portfolio', getInvestmentPortfolioByRetirementId);
+router.post('/api/investment/portfolio', createInvestmentPortfolio);
+router.get('/api/user/:id/investment/portfolios', getAllInvestmentPortfolioByUserId);
+router.get('/api/investment/portfolio/:id', getInvestmentPortfolioById);
+router.patch('/api/investment/portfolio/:id', editInvestmentPortfolio);
+router.delete('/api/investment/portfolio/:id', deleteInvestmentPortfolio)
+router.get('/api/emergency/:id/investment/portfolio', getInvestmentPortfolioByEmergencyId);
+router.get('/api/goal/:id/investment/portfolio', getInvestmentPortfolioByGoalId);
+router.get('/api/retirement/:id/investment/portfolio', getInvestmentPortfolioByRetirementId);
 
-router.get('/investment/portfolio/:id/allocation', getInvestmentPortfolioAllocationByPortfolioId);
-router.patch('/investment/portfolio/:id/allocation', editInvestmentPortfolioAllocationByPortfolioId);
-router.post('/investment/portfolio/:id/transaction', addInvestmentTransaction);
-router.get('/investment/:id/transactions', getAllInvestmentTransactionsByPortfolioId);
-router.get('/investment/transaction/:id', getInvestmentTransactionById);
-router.post('/emergency/investment/portfolio/:id/transaction', addEmergencyInvestmentTransaction);
-router.post('/goal/investment/portfolio/:id/transaction', addGoalInvestmentTransaction);
-router.post('/retirement/investment/portfolio/:id/transaction', addRetirementInvestmentTransaction);
+router.get('/api/investment/portfolio/:id/allocation', getInvestmentPortfolioAllocationByPortfolioId);
+router.patch('/api/investment/portfolio/:id/allocation', editInvestmentPortfolioAllocationByPortfolioId);
+router.post('/api/investment/portfolio/:id/transaction', addInvestmentTransaction);
+router.get('/api/investment/:id/transactions', getAllInvestmentTransactionsByPortfolioId);
+router.get('/api/investment/transaction/:id', getInvestmentTransactionById);
+router.post('/api/emergency/investment/portfolio/:id/transaction', addEmergencyInvestmentTransaction);
+router.post('/api/goal/investment/portfolio/:id/transaction', addGoalInvestmentTransaction);
+router.post('/api/retirement/investment/portfolio/:id/transaction', addRetirementInvestmentTransaction);
 
-router.post('/investment/portfolio/fund', addMutualFundToInvestmentPortfolio);
-router.post('/investment/portfolio/funds', addMutualFundsToInvestmentPortfolio);
+router.post('/api/investment/portfolio/fund', addMutualFundToInvestmentPortfolio);
+router.post('/api/investment/portfolio/funds', addMutualFundsToInvestmentPortfolio);
 
 export default router;

803d1bc422e81d48dd9d5be1394d17a73159262b
Ed1n <Jdei_nin230@hotmail.com>
2023-05-14 22:27:43 +0700

    adjust pass in sequlize

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('money_mate_db_test', 'root', 'mysql@Admin007', {
+const db = new Sequelize('money_mate_db_test', 'root', '', {
     host:"localhost",
     dialect: "mysql",
 })
